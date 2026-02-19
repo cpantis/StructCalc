3539396a02a919b161681996754cb13d6ff25dc7
cpantis <calin_pantis@yahoo.com>
2026-02-19 16:29:34 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StructCalcAI from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StructCalcAI />
+  </React.StrictMode>
+);
